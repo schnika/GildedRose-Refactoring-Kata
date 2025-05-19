78eb94bcd4a1ede1d45279b6d0de842ec47acc84
Lisa Hilterhaus <lisa.hilterhaus@1komma5grad.com>
2025-05-19 11:18:32 +0200

    Squashed commit of the following:
    commit 4f1b0e48393332168fbd5d18791cd2450c69ebe3
    Author: Lisa Hilterhaus <lisa.hilterhaus@1komma5grad.com>
    Date:   Mon May 19 11:18:13 2025 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:TypeScript/test/jest/gilded-rose.spec.ts
    
    commit 3a9e531e9e1fdf7c019c2d7549990546c5c825cf
    Author: Vincent Oosterwijk <vincent.oosterwijk@1komma5grad.com>
    Date:   Mon May 19 11:02:05 2025 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:TypeScript/test/jest/gilded-rose.spec.ts
    
    commit ee1958195a584118d15581b8baa97a14911d5313
    Author: Marcel Elsterit <marcel.elstereit@1komma5grad.com>
    Date:   Mon May 19 10:48:04 2025 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:TypeScript/.tool-versions
    
    commit 90922536c08b703aadb43dd72a53c5cf2e872d79
    Author: Patrick Nüser <patrick.nueser@1komma5grad.com>
    Date:   Mon May 19 10:25:08 2025 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:TypeScript/test/jest/__snapshots__/approvals.spec.ts.snap
    Co-authored-by: Patrick Nüser <patrick.nueser@1komma5grad.com>
    Co-authored-by: Marcel Elsterit <marcel.elstereit@1komma5grad.com>
    Co-authored-by: Vincent Oosterwijk <vincent.oosterwijk@1komma5grad.com>

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,74 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe("Gilded Rose", () => {
+  it("should foo", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe("foo");
+  });
+
+  describe("Sulfuras", () => {
+    it("quality should never change", () => {
+      const gildedRose = new GildedRose([
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(80);
+    });
+
+    it("sellIn should never change", () => {
+      const gildedRose = new GildedRose([
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(0);
+    });
+  });
+
+  describe("Aged Brie", () => {
+    it("quality should increase", () => {
+      const gildedRose = new GildedRose([new Item("Aged Brie", 10, 1)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(2);
+    });
+
+    it ("quality should not exceed 50", () => {
+      const gildedRose = new GildedRose([new Item("Aged Brie", 10, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+    });
+  });
+
+  describe("Normal Item", () => {
+
+    it ("quality should decrease", () => {
+      const gildedRose = new GildedRose([new Item("Normal Item", 10, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(9);
+    });
+    it ("quality should not be negative", () => {
+      const gildedRose = new GildedRose([new Item("Normal Item", 10, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    });
+
+    it ("sellIn should decrease", () => {
+      const gildedRose = new GildedRose([new Item("Normal Item", 10, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(9);
+    });
+
+    it ("quality should decrease twice as fast after sellIn date", () => {
+      const gildedRose = new GildedRose([new Item("Normal Item", 0, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(8);
+    });
+
+    // it ("quality should not be more than 50", () => {
+    //   const gildedRose = new GildedRose([new Item("Normal Item", 0, 80)]);
+    //   const items = gildedRose.updateQuality();
+    //   expect(items[0].quality).toBe(49);
+    // });
   });
 });
+
